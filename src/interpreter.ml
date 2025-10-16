f347dd0ba2eac9581710575440768478aa090cd7
Aaron Massey <03MasseyAaron@gmail.com>
2025-10-16 16:09:19 -0500

    Commented half the functions

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -26,17 +26,17 @@ let is_letter c = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
 let is_digit c = c >= '0' && c <= '9'
 
 let is_valid_name (s : string) : bool = (*Aaron Massey*)
-  if String.length s = 0 then false
+  if String.length s = 0 then false (*If the string is empty, it is not a valid name*)
   else
-    let first_char = s.[0] in
-    if not (is_letter first_char || first_char = '_') then
+    let first_char = s.[0] in 
+    if not (is_letter first_char || first_char = '_') then (* The first character must be a letter or '_'*)
       false
     else
       let rec check_rest i = 
         if i >= String.length s then true
         else
           let char = s.[i] in
-          if is_letter char || is_digit char || char = '_' then
+          if is_letter char || is_digit char || char = '_' then (*Every element must be either a digit, letter, or '_'*)
           check_rest (i + 1)
           else
             false
@@ -136,54 +136,54 @@ let pop (stk: stack) : stack =  (*Aaron Massey*) (*Might need to rework*)
 
 let add (stk : stack) : stack = (*Aaron Massey*)
   match stk with
-    | Int a :: Int b :: rest ->
+    | Int a :: Int b :: rest -> (*If there are two Ints then add them*)
       pushInt (a + b) rest
-    | Int a :: rest ->
+    | Int a :: rest -> (*If there is only one Int, push it back onto the stack with an error*)
       pushError (Int a :: rest)
-    | _ -> pushError stk
+    | _ -> pushError stk (*If there is no Int, push an error*)
 
 let sub (stk : stack) : stack = (*Aaron Massey*)
   match stk with
-    | Int a :: Int b :: rest ->
+    | Int a :: Int b :: rest -> (*If there are two Ints, subtract them*)
       pushInt (b - a) rest
-    | Int a :: rest ->
+    | Int a :: rest -> (*If there is only one Int, push it back onto the stack with an error*)
       pushError (Int a :: rest)
-    | _ -> pushError stk
+    | _ -> pushError stk (*If there are no Ints, push an error onto the stack*)
 
 let mult (stk : stack) : stack = 
   match stk with
-    | Int a :: Int b :: rest ->
+    | Int a :: Int b :: rest ->(*If there are two Ints, multiply them*)
       pushInt (a * b) rest
-    | Int a :: rest ->
+    | Int a :: rest -> (*If there is only one Int, push it back onto the stack with an error*)
       pushError (Int a :: rest)
-    | _ -> pushError stk
+    | _ -> pushError stk (*If there are no Ints. push an error onto the stack*)
 
 let div (stk : stack) : stack = 
   match stk with
-    | Int a :: Int b :: rest ->
-      if a = 0 then
+    | Int a :: Int b :: rest -> (*If there are two Ints, divide them *)
+      if a = 0 then (*If the denominator is 0, push the Ints back onto the stack with an error*)
         pushError (Int a :: Int b :: rest)
       else
-        pushInt (b / a) rest
+        pushInt (b / a) rest (*If the Ints are valid, divide them*)
     | Int a :: rest ->
-      pushError (Int a :: rest)
-    | _ -> pushError stk
+      pushError (Int a :: rest) (*If there is only one Int, push it back onto the stack with an error*)
+    | _ -> pushError stk (*If there are no Ints, push an error to the stack*)
 
 let rem (stk : stack) : stack = 
   match stk with
-    | Int a :: Int b :: rest ->
+    | Int a :: Int b :: rest -> (*If there are two Ints, get the modulo*)
       if a = 0 then
-        pushError (Int a :: Int b :: rest)
+        pushError (Int a :: Int b :: rest) (*If the denominator is 0, return the Ints to the stack and push an error*)
       else
-        pushInt (b mod a) rest
+        pushInt (b mod a) rest (* Otherwise push the modulo (remainder) of the Ints*)
     | Int a :: rest ->
-      pushError (Int a :: rest)
-    | _ -> pushError stk
+      pushError (Int a :: rest) (*If there is only one Int push it to the stack with an error*)
+    | _ -> pushError stk (*If there are no Ints push an error to the stack*)
 
 let sign (stk : stack) : stack = 
   match stk with
-    | Int a :: rest -> pushInt (a * -1) stk
-    | _ -> pushError stk
+    | Int a :: rest -> pushInt (a * -1) rest
+    | _ -> pushError stk 
 
 let swap (stk : stack) : stack = (*Aaron Massey*)
   match stk with
